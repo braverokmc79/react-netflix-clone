44356052be67e8921e09575dd0334f17f54a3e7d
braverokmc79 <braverokmc79@gmail.com>
2023-02-07 00:51:21 +0900

    43.Styled Component를 이용한 비디오 배너 생성하기

---
+++ src/api/axiosEn.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import API_KEY from "./apiKey";
+
+const instance = axios.create({
+
+    baseURL: "https://api.themoviedb.org/3",
+    params: {
+        api_key: API_KEY,
+        language: "en-US",
+    }
+
+});
+
+export default instance;
+
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../api/axios';
+import axiosEn from '../api/axiosEn';
 import requests from '../api/requests';
 import './Banner.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+    background:black;
+`;
+
+const HomeContainer = styled.div`
+    width: 100%;
+    height: 100%;
+   
+`;
+
+const Iframe = styled.iframe`
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    opacity: 0.65;
+    border: none;
+
+    &::after{
+       content:"" ;
+       position: absolute;
+       top: 0;
+       left: 0;
+       width: 100%;
+       height: 100%;       
+    }
+`;
 
 const Banner = () => {
     const [movie, setMovie] = useState([]);
+    const [movieKey, setMovieKey] = useState("");
+    const [isClicked, setIsClicked] = useState(false);
 
     useEffect(() => {
         fetchData();
@@ -15,22 +52,32 @@ const Banner = () => {
         //현재 상영중인 영화 정보를 가져오기(여러 영화)
         const request = await axios.get(requests.fetchNowPlaying);
 
+        const results = request.data.results.filter(data => {
+            return data.overview.length > 1
+        })
+
+        console.log(" results", results);
+
         //여러 영화 중 영화 하나의 ID 를 가져오기
-        const movieId =
-            request.data.results[Math.floor(Math.random() * request.data.results.length)].id;
+        const movieId = results[Math.floor(Math.random() * results.length)].id;
 
         //특정 영화의 더 상세한 정보를 가져오기 (비디오 정보도 포함)
-        const { data: movieDetail } = await axios.get(`/movie/${movieId}`,
-            {
-                params: { append_to_response: "videos" }
-            }
-        );
+        const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+            params: { append_to_response: "videos" },
+        });
 
-        setTimeout(() => {
-            setMovie(movieDetail);
-        }, 100);
+        setMovie(movieDetail);
 
-        console.log("movie3  : ", movie);
+
+        //비디오가 없다면 다음을 실행
+        if (movie.videos === undefined) {
+            const { data: movieDetailEn } = await axiosEn.get(`movie/${movieId}`, {
+                params: { append_to_response: "videos" },
+            });
+            setMovieKey(movieDetailEn.videos.results[0].key);
+        } else {
+            setMovieKey(movie.videos.results[0].key)
+        }
 
     };
 
@@ -38,34 +85,60 @@ const Banner = () => {
         return str?.length > n ? str.substr(0, n - 1) + "..." : str;
     }
 
-    return (
-        <header
-            className="banner"
-            style={{
-                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-                backgroundPosition: "top center",
-                backgroundSize: "cover",
-            }}
-        >
-            <div className="banner__contents">
-                <h1 className="banner__title">
-                    {movie.title || movie.name || movie.original_name}
-                </h1>
 
-                <div className="banner__buttons">
-                    <button className="banner__button play">
-                        Play
-                    </button>
-                    <button className="banner__button info">More Information</button>
+
+    if (!isClicked) {
+        return (
+            movie.backdrop_path && <header
+                className="banner"
+                style={{
+                    backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+                    backgroundPosition: "top center",
+                    backgroundSize: "cover",
+                }}
+            >
+                <div className="banner__contents">
+                    <h1 className="banner__title">
+                        {movie.title || movie.name || movie.original_name}
+                    </h1>
+
+                    <div className="banner__buttons">
+                        <button className="banner__button play" onClick={() => setIsClicked(true)}>
+                            Play
+                        </button>
+
+                        <button className="banner__button info">More Information</button>
+                    </div>
+
+                    <h1 className="banner__description">
+                        {truncate(movie.overview, 100)}
+                    </h1>
                 </div>
+                <div className="banner--fadeBottom" />
+            </header>
+        );
 
-                <h1 className="banner__description">
-                    {truncate(movie.overview, 100)}
-                </h1>
-            </div>
-            <div className="banner--fadeBottom" />
-        </header>
-    );
+    } else {
+
+
+        return (
+            <Container >
+                <HomeContainer>
+                    <Iframe
+                        width="640"
+                        height="360"
+                        src={`https://www.youtube.com/embed/${movieKey}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movieKey}`}
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="autoplay; fullscreen"
+                        allowfullscreen
+                    ></Iframe>
+                </HomeContainer>
+
+            </Container >
+
+        );
+    }
 };
 
 export default Banner;
